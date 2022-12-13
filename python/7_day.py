87882c64c81218982b79a77d852c4d547a2a32c9
Alex Jones <amjones@live.co.uk>
2022-12-13 22:23:12 +0000

    Solution to D7

--- python/7_day.py
+++ python/7_day.py
@@ -4,73 +4,52 @@ import utils
 # Global variables
 DAY = 7
 TEST_ANSWER_ONE = 95437
-TEST_ANSWER_TWO = None
+TEST_ANSWER_TWO = 24933642
 
 
-class Dir:
-    def __init__(self, id, parent):
-        self.id = id
-        self.parent = parent
-        self.files = dict()
-        self.dirs = set()
-    
-
-    def size(self):
-        print(self.id)
-        size_files = sum(self.files.values())
-        size_dirs = size_files
-        for dir in self.dirs:
-            size_dirs += dir.size()
-        return size_dirs
-    
-
-    def list_dir_ids(self):
-        dirs = [dir.id for dir in self.dirs]
-        for dir in self.dirs:
-            dirs += dir.list_dir_ids()
-        return dirs
-
-
-def parse_input(lines: list):
-    dirs = {'/': Dir('/', '/')}
-    current_dir = dirs['/']
+def parse_input(lines):
+    dirs = {'/': 0}
+    dirs_tracking = ['/']
     for line in lines:
         if line.startswith('$ cd'):
-            id = line.split('$ cd ')[1]
-            if id == '..':
-                current_dir = dirs[current_dir.parent]
+            id = line.split()[2]
+            if id == '/':
+                dirs_tracking = ['/']
+            elif id == '..':
+                dirs_tracking.pop()
             else:
-                current_dir = dirs[id]
+                full_id = '.'.join(dirs_tracking) + f'.{id}'
+                dirs[full_id] = 0
+                dirs_tracking.append(full_id)
         elif line.startswith('$ ls'):
-            pass
-        elif line.startswith('dir '):
-            id = line.split('dir ')[1]
-            if not id in dirs:
-                dirs[id] = Dir(id, current_dir.id)
-            if not id in current_dir.dirs:
-                current_dir.dirs.add(dirs[id])
-        elif line.strip():
-            size, id = line.split()
-            if not id in current_dir.files:
-                current_dir.files[id] = int(size)
+            continue
+        elif line.startswith('dir'):
+            continue
+        else:
+            size = line.split()[0]
+            for dir in dirs_tracking:
+                dirs[dir] += int(size)
     return dirs
 
 
 def solve_part_one(lines: list):
-    dirs = parse_input(lines)
     size = 0
-    for dir in dirs.values():
-        if dir.id != '/' and dir.size() <= 100000:
-            print(dir.id, dir.size())
-            size += dir.size()
-            print(size)
-            if dir.id == "clm":
-                raise AssertionError
+    dirs = parse_input(lines)
+    for dir in dirs:
+        if dirs[dir] < 100000:
+            size += dirs[dir]
     return size
 
 
 def solve_part_two(lines: list):
-    pass
+    dirs = parse_input(lines)
+    space_available = 70000000 - dirs['/']
+    space_required = 30000000 - space_available
+    candidate_dir = '/'
+    for dir in dirs:
+        if dirs[dir] >= space_required and dirs[dir] < dirs[candidate_dir]:
+            candidate_dir = dir
+    return dirs[candidate_dir]
 
 
 if __name__ == "__main__":
